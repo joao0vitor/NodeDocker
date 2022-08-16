d85ffa04d5b829a0d9690860f0e14f1644c82545
JOAO VITOR DA SILVA SOUZA <joao.vssouza42@senacsp.edu.br>
2022-08-16 16:41:29 -0300

    Rotas do usuario criadas

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 const conexao = mysql.createConnection({
-    host: "127.0.0.1",
+    host: "172.17.0.1",
     port: "6520",
     user: "root",
     password: "123@senac",
@@ -25,6 +25,46 @@ app.get("/usuarios/listar",(req,res)=>{
         if(erro)return res.status(500).send({output:`Erro -> ${erro}`});
         res.status(200).send({ output: dados });
     });
+});
+
+app.post("/usuarios/cadastro", (req, res) =>{
+if(req.body.nomeusuario == "" || 
+   req.body.senha === "" || 
+   req.body.email == ""  ||
+   req.body.nomecompleto == "" ||
+   req.body.cpf == "" ||
+   req.body.foto == ""
+   ){
+       return res.status(400).send({ output: `Você deve passar todos os dados`})
+   }
+   conexao.query("insert int usuario set ?", req.body,(error, data) =>{
+       if(erro) return res.status(500).send({output : `Erro ao tentar cadastrar -> ${error}`});
+       res.status(201).send({output: `Usuários cadastrado`,dados:data});
+   });
+});
+
+app.post("/usuarios/login",(req,res) =>{
+ if(req.body.usuario == "" ||
+    req.body.senha == ""){
+        return res.status(400).send({output: `Você deve passar todos os dadods`});
+    }
+    conexao.query("Select * from usuario where nomeusuario=? and senha=?", 
+    [req.body.nomeusuario, req.body.senha],(error, data) =>{
+        if(error) return res.status(500).send({output: `Erro ao tentar logar -> ${error}`})
+        res.status(200).
+        send({output: `Logado`, dados:data})
+    }
+  );
+});
+
+
+app.put("/usuarios/atualizar/:id",(req,res)=>{
+    conexao.query("update usuario set ? where id=?",
+    [req.body,req.params.id], (error,data)=>{
+        if(error) return res.status(500).send({output: `Erro ao tentar atualizar -> ${error}`});
+        res.status(200).send({output: `Atualizado`, dados:data});
+    }
+   );
 });
 
 app.listen("3000", () => console.log("Servidor online"));
